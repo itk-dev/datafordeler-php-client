66f77cf51704717c0da0d5db4f5110bc3e173028
Mikkel Ricky <rimi@aarhus.dk>
2021-09-21 20:08:57 +0200

    Cleaned up

--- src/CVR/HentCVRData/v1/Client.php
+++ src/CVR/HentCVRData/v1/Client.php
@@ -4,10 +4,17 @@ namespace ItkDev\Datafordeler\CVR\HentCVRData\v1;
 
 use ItkDev\Datafordeler\AbstractClient;
 
+/**
+ * HentCVRData client
+ *
+ * @see https://confluence.datafordeler.dk/pages/viewpage.action?pageId=17139087#REST(CVR)-REST-HentCVRData
+ */
 class Client extends AbstractClient
 {
     /**
      * {@inheritdoc}
+     *
+     * @see https://datafordeler.dk/dataoversigt/det-centrale-virksomhedsregister-cvr/hentcvrdata/.
      */
     protected function getApiBaseUrl(): string
     {
@@ -17,16 +24,42 @@ class Client extends AbstractClient
     }
 
     /**
-     * Hent virksomhed med CVR nummer.
-     *
-     * @param string $cvrNummer The CVR number.
+     * https://confluence.datafordeler.dk/pages/viewpage.action?pageId=17139087#REST(CVR)-Metode-hentAndredeltagereMedCVREnhedsid
+     */
+
+    /**
+     * https://confluence.datafordeler.dk/pages/viewpage.action?pageId=17139087#REST(CVR)-Metode-hentCVRPersonMedCVREnhedsid
+     */
+
+    /**
+     * https://confluence.datafordeler.dk/pages/viewpage.action?pageId=17139087#REST(CVR)-Metode-hentEnhedMedCVREnhedsid
+     */
+
+    /**
+     * https://confluence.datafordeler.dk/pages/viewpage.action?pageId=17139087#REST(CVR)-Metode-hentProduktionsenhedMedCVREnhedsid
+     */
+
+    /**
+     * https://confluence.datafordeler.dk/pages/viewpage.action?pageId=17139087#REST(CVR)-Metode-hentProduktionsenhedMedPNummer
+     */
+
+    /**
+     * https://confluence.datafordeler.dk/pages/viewpage.action?pageId=17139087#REST(CVR)-Metode-hentVirksomhedMedCVREnhedsid
+     */
+
+
+    /**
+     * @param string|array $cvrOrParameters The CVR number or an array with parameters (cf. <a href="https://confluence.datafordeler.dk/pages/viewpage.action?pageId=17139087#REST(CVR)-Metode-hentVirksomhedMedCVRNummer">hentVirksomhedMedCVRNummer</a>)
      *
      * @throws \GuzzleHttp\Exception\GuzzleException
+     *
+     * @see https://confluence.datafordeler.dk/pages/viewpage.action?pageId=17139087#REST(CVR)-Metode-hentVirksomhedMedCVRNummer
      */
-    public function hentVirksomhedMedCVRNummer(string $cvrNummer): array
+    public function hentVirksomhedMedCVRNummer($cvrOrParameters): array
     {
-        return $this->invoke(__FUNCTION__, [
-            'pCVRNummer' => $cvrNummer,
-        ]);
+        return $this->invoke(
+            __FUNCTION__,
+            !is_array($cvrOrParameters) ? ['pCVRNummer' => $cvrOrParameters] : $cvrOrParameters
+        );
     }
 }
