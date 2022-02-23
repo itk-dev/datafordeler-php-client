619145e7ebe07a1c59084abfa481f9dd835984c5
Mikkel Ricky <rimi@aarhus.dk>
2022-02-23 15:44:58 +0100

    Added husnummerTilBygningBfe

--- src/Service/DAR/V1/DAR_BFE_Public.php
+++ src/Service/DAR/V1/DAR_BFE_Public.php
@@ -9,14 +9,6 @@ use ItkDev\Datafordeler\Service;
  *
  * Uses magic to invoke these service methods.
  *
- * @method adresse(array $parameters): array
- * @method Husnummer(array $parameters): array
- * @method husnummerTilAdresse(array $parameters): array
- * @method husnummerTilJordstykke(array $parameters): array
- * @method Navngivenvej(array $parameters): array
- * @method NavngivenvejKommunedel(array $parameters): array
- * @method Postnummer(array $parameters): array
- * @method Supplerendebynavn(array $parameters): array
  */
 class DAR_BFE_Public extends Service
 {
@@ -26,6 +18,19 @@ class DAR_BFE_Public extends Service
     protected function getApiBaseUrl(): string
     {
         return 'https://services.datafordeler.dk/DAR/DAR_BFE_Public/1/rest/';
+    }
+
+    /**
+     * @see https://confluence.sdfe.dk/pages/viewpage.action?pageId=16056323#REST(DAR)-Metode-husnummerTilBygningBfe
+     */
+    public function husnummerTilBygningBfe(string $husnummerId): array
+    {
+        return $this->invoke(
+            __FUNCTION__,
+            [
+                'husnummerId' => $husnummerId,
+            ]
+        );
     }
 
     /**
